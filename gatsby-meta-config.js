4f10123307dd88212dda565553d54a8a9a622be3
jungjaechoi <joong8812@gmail.com>
2022-03-19 23:28:24 +0900

    add - comment feature

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,7 +6,7 @@ module.exports = {
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `joong8812/joong8812.github.io`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
